c0e54c46cc6059b81a0dc3f7f73ee21033305334
Corey <coreybeard2009@outlook.com>
2026-07-16 18:06:21 +0100

    hud updated slightly

--- mainn.py
+++ mainn.py
@@ -227,39 +227,5 @@ def mainpage():
             conn.commit()
 
             messagebox.showinfo("Success", "All previous game data has been deleted.")
-        # Get all clubs used from the table
-        # cursor.execute("SELECT Clubs_used FROM game")
-        # clubs = cursor.fetchall()
-        #
-        # # Simplify list for changes
-        # club_list = [club[0].lower() for club in clubs]
-        #
-        # # Remove putter since its always used
-        # club_list = [club for club in club_list if club != "putter"]
-        #
-        # # Find most common club
-        # if club_list:
-        #     mostusedclub = Counter(club_list).most_common(1)[0][0]
-        # else:
-        #     mostusedclub = None
-        #
-        # print("Most used club (excluding putter):", mostusedclub)
-        # # Take lowest 10 values
-        # lowest_10 = sorted(gamelist)[:10]
-        #
-        # # Calculate handicap
-        # handicap = sum(lowest_10) * 0.96
-        #
-        # cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-        # print(cursor.fetchall())
-        #
-        # print("Tables in DB:")
-        # cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-        # for table in cursor.fetchall():
-        #     print(table)
-        #
-        #
-        #
-        #
 
 
